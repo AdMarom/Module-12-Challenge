0a8d64ce669272872638792cdc736896e942714d
AdMarom <adenamarom@gmail.com>
2023-03-01 22:11:24 -0800

    view all departments, roles, employees working

--- index.js
+++ index.js
@@ -18,18 +18,45 @@ db.connect(function(err){
 })
 
 //Connection query will go here. refer to https://www.npmjs.com/package/mysql2
-
-
 //inquirer prompt questions refer to https://www.npmjs.com/package/inquirer/v/8.2.4
+
 function menu() {
-inquirer.prompt()
+
+inquirer.prompt( [
+    {
+        type: 'list',
+        name: 'view_options',
+        message: 'What would you like to do?',
+        choices: ['view all departments', 'view all roles','view all employees','add a department','add a role', 'add an employee','update an employee role'],
+    },
+])
 .then((answers) => {
+    if(answers.view_options === 'view all departments') {
+       db.query(`SELECT * FROM department`,
+       function(err, results) {
+        console.table(results);
+        return menu();
+       })
+    } else if (answers.view_options === 'view all roles') {
+        db.query(`SELECT * FROM role`,
+         function(err, results) {
+        console.table(results);
+        return menu();
+       })
+    } else { (answers.view_options === 'view all employees')
+        db.query(`SELECT * FROM employee`,
+         function(err, results) {
+        console.table(results);
+        return menu();
+       })
+    };
 
-})
-.catch((error) => {
-    if(error) {
-    } else {
 
-    }
 });
-}
+// .catch((error) => {
+//     if(error) {
+//     } else {
+
+//     }
+// });
+ }
